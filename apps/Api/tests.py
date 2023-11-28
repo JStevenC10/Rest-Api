7f2316387e0053ba284cb5a0da269a5b4da39537
ChiquiPR <johansteven.h10@gmail.com>
2023-11-28 12:12:41 -0500

    test gender

--- apps/Api/tests.py
+++ apps/Api/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
+from .models import Gender
 # Create your tests here.
+
+class GenderTestCase(TestCase):
+
+    def setUp(self):
+        self.gender = Gender.objects.create(name='Test gender', state=True)
+
+    def test_gender_created(self):
+        self.assertGreater(len(self.gender.name), 3)
+        self.assertEqual(self.gender.state, True)
+        # self.assertEquals(self.gender.name, 'holamundo')
+        self.assertTrue(self.gender.state)
